3ca5d3516969a5f51b039901419e61563c6d9c28
OneDamien <damien.r@live.com>
2016-05-08 13:43:04 -0300

    Nana beubeubeu

--- dm.js
+++ dm.js
@@ -1,6 +1,11 @@
 fs = require('fs');
 
-fs.readdir("/home"+ +"/Downloads", function (err, files) {
+var sys = require('sys')
+var exec = require('child_process').exec;
+function puts(error, stdout, stderr) { sys.puts(stdout) }
+exec("whoami",puts);
+
+fs.readdir("/home/"+ +"/Downloads", function (err, files) {
   if (err) {
     console.log(err);
     return;
