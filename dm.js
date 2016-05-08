d0ecf812f6f3968f0b12e34b53ba093d02d2aba7
OneDamien <damien.r@live.com>
2016-05-08 14:44:25 -0300

    Create Arrays with file extensions and paths

--- dm.js
+++ dm.js
@@ -1,18 +1,19 @@
 fs = require('fs');
-
+var pathExt = [
+["../Pictures/",".png",".jpg",".gif",".jpeg"]
+["../Documents/",".doc",".txt",".pdf",".xls",".docx",".ppt"]
+["../Development/",".js",".java"]
+["../Music/",".mp3",".wav"]
+["../Videos/",".mp4",".mov"]
+];
 var child = require("child_process");
 var name;
 child.exec("whoami", function (error, stdout, stderr) {
-
   //stdout and stderr are available here
-  name = stdout;
+name = stdout;
 name = name.replace(/\r?\n|\r/g, "");
 
 fs.readdir("/home/"+name+"/Downloads", function (err, files) {
-  if (err) {
-    console.log(err);
-    return;
-  }
   console.log(files);
 });
 });
